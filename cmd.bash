71242104aa812fd765111122e0fdc653c8f61c29
Israiloff <stardustaindark@gmail.com>
2024-02-22 12:43:44 +0500

    removed: unused build script;

--- cmd.bash
+++
@@ -1,3 +0,0 @@
-declare BUILD_VERSION=0.0.4
-docker build --build-arg JDK_VERSION=21 --build-arg PYTHON_VERSION=3 -t israiloff/lvim:$BUILD_VERSION . &&
-docker run -it -d --name lvim israiloff/lvim:$BUILD_VERSION
